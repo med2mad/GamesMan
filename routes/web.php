76a4e0848c2f20ab89b1ca4b9fc793360dcba823
watawata <mohamed.leghdaich@gmail.com>
2024-07-26 22:48:46 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view ("welcome", ['t'=>[ 'name'=>'flipper cube' , 'link'=>'game link' , 'query'=>request('zz') ]]);
+});
