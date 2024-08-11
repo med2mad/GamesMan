93cd6f4c146c1f8369242f1f245a04bcc1f0c0d1
watawata <mohamed.leghdaich@gmail.com>
2024-08-11 06:52:31 +0100

    embed games

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -10,20 +10,37 @@
             player.load("/games/{{$file}}.swf");
         });
     </script>
-    <script src="/ruffle/ruffle.js"></script>
+    <!-- <script src="/ruffle/ruffle.js"></script> -->
+
+     <div class="game">
+
+
     
- 
-    <!--
     
-    <iframe src="https://www.crazygames.com/embed/air-battle-royale-sky-blitz" width="750" height="750" frameborder="0" allow="gamepad *;"></iframe>
-    <iframe src="https://html5.gamemonetize.co/kdivqdv571za5rmwci39pen45ktk4luo/" width="800" height="600" scrolling="none" frameborder="0"></iframe>
-    <iframe src="https://html5.gamemonetize.co/i99lrifnezfrvjt692cm91mblkkro6na/" width="400" height="800" scrolling="none" frameborder="0"></iframe>
-    <table style="margin:0 0 10px 0; width:244px; background:#fff; border:1px solid #F3F3F3;" cellspacing="0" cellpadding="0"><tr><td style="font-family:verdana; font-size:11px; color:#000; padding:5px 5px;"><iframe allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" style="width:968px;height:561px;" src="https://html5.gamedistribution.com/c99a523e92ed42de95809f52f7508e12/" width="968px" height="561px" frameborder="0" scrolling="no"></iframe></td></tr></table>
-    <table style="margin:0 0 10px 0; width:244px; background:#fff; border:1px solid #F3F3F3;" cellspacing="0" cellpadding="0"><tr><td style="font-family:verdana; font-size:11px; color:#000; padding:5px 5px;"><iframe allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" style="width:968px;height:561px;" src="https://html5.gamedistribution.com/5b1cb82de3324264912fe78c015fafa0/" width="968px" height="561px" frameborder="0" scrolling="no"></iframe></td></tr></table>
-    <iframe src="https://html5.gamemonetize.co/1oa8q7ngksa2dfiy7lhfqitbq75i2k1j/" width="750" height="1334" scrolling="none" frameborder="0"></iframe>
-    <iframe src="https://html5.gamemonetize.co/tvmfhxxrzahsek5v97goloybsrswo07s/" width="1280" height="720" scrolling="none" frameborder="0"></iframe>
-    <iframe src="https://www.crazygames.com/embed/fleeing-the-complex"  width="560" height="384" frameborder="0" allow="gamepad *;"></iframe>
-    <iframe src="https://html5.gamemonetize.co/qmiysm6xxk90nbsv387jo7v6jooth9c7/" width="800" height="500" scrolling="none" frameborder="0"></iframe>
--->
+
+    </div>
+
+    
+    
+
+    <iframe src="https://html5.gamemonetize.co/i9254gatv0wn1x2itk7wdp3g6qixhznj/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://html5.gamemonetize.co/0zkh910goccyxzzy132k8cizu1chqclr/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://html5.gamemonetize.co/ywgsu1e8jqboj8d4tbnomnm111ro0vpy/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://html5.gamemonetize.co/8x3gofq0i4jtnlby1a6s0gsknozmgmno/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://html5.gamemonetize.co/pp7tmbpqit64nihyivf1spiliurjr0rh/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://html5.gamemonetize.co/530wo4p5zdphord3wk4byh8wdv751z57/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://html5.gamemonetize.co/c2n8tk67k327x0qt4g0l28xob471ot51/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://html5.gamemonetize.co/47lce5e4kb51icu8gbckm8x39aiztmuf/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://html5.gamemonetize.co/zmxjbnv63ylnzq5qvynsjj9e9h9c5mqr/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://www.crazygames.com/embed/air-battle-royale-sky-blitz" width="960" height="600" frameborder="0" allow="gamepad *;"></iframe>
+    <iframe src="https://html5.gamemonetize.co/kdivqdv571za5rmwci39pen45ktk4luo/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://html5.gamemonetize.co/i99lrifnezfrvjt692cm91mblkkro6na/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://html5.gamedistribution.com/c99a523e92ed42de95809f52f7508e12/" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" style="width:968px;height:561px;" width="960" height="600" frameborder="0" scrolling="no"></iframe>
+    <iframe src="https://html5.gamedistribution.com/5b1cb82de3324264912fe78c015fafa0/" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" style="width:968px;height:561px;" width="960" height="600" frameborder="0" scrolling="no"></iframe>
+    <iframe src="https://html5.gamemonetize.co/1oa8q7ngksa2dfiy7lhfqitbq75i2k1j/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://html5.gamemonetize.co/tvmfhxxrzahsek5v97goloybsrswo07s/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+    <iframe src="https://www.crazygames.com/embed/fleeing-the-complex" width="960" height="600" frameborder="0" allow="gamepad *;"></iframe>
+    <iframe src="https://html5.gamemonetize.co/qmiysm6xxk90nbsv387jo7v6jooth9c7/" width="960" height="600" scrolling="none" frameborder="0"></iframe>
+
 
 @include( 'footer' );
