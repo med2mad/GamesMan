5904eaffff1240be855b93214bb6e80fec5fb626
watawata <mohamed.leghdaich@gmail.com>
2024-08-21 00:25:57 +0100

    update search and pagination

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -1,21 +1,35 @@
 @include( 'partials.header' )
 
-<div style="display:flex; justify-content:space-between;">
+<div class="search">
 	<div>
 		<form action="/page/games" method="GET">
-			<label for="title">Search : </label>
-			<input name="title" value="{{$title}}" type="text" id="title"/>
-			<label for="sortby">Sort by : </label>
-			<select name="sortby" id="sortby">
-				<option value="popularity" {{$sortby=='popularity'?'selected':''}}>Popularity</option>
-				<option value="title" {{$sortby=='title'?'selected':''}}>Title</option>
-				<option value="genre" {{$sortby=='genre'?'selected':''}}>Genre</option>
-			</select>
-			<select name="order">
-				<option value="asc" {{$order=='asc'?'selected':''}}>Asc</option>
-				<option value="desc" {{$order=='desc'?'selected':''}}>Desc</option>
-			</select>
-			<input type="submit" value="Search"/>
+			<div class="row" style="align-items:flex-end;">
+				<div class="col-5">
+					<label for="title">Search : </label>
+					<input name="title" value="{{$title}}" type="text" id="title" class="form-control" placeholder="Title"/>
+				</div>
+				<div class="col-4">
+					<label for="sortby">Sort by : </label>
+					<div class="row">
+						<div class="col-7 p-0">
+							<select name="sortby" id="sortby" class="form-control">
+								<option value="popularity" {{$sortby=='popularity'?'selected':''}}>Popularity</option>
+								<option value="title" {{$sortby=='title'?'selected':''}}>Title</option>
+								<option value="genre" {{$sortby=='genre'?'selected':''}}>Genre</option>
+							</select>
+						</div>
+						<div class="col-4 p-0">
+							<select name="order" class="form-control">
+								<option value="asc" {{$order=='asc'?'selected':''}}>Asc</option>
+								<option value="desc" {{$order=='desc'?'selected':''}}>Desc</option>
+							</select>
+						</div>
+					</div>
+				</div>
+				<div class="col-2">
+					<input type="submit" value="Search" class="btn btn-primary-hover-outline" />
+				</div>
+			</div>
 		</form>
 	</div>
 
@@ -52,5 +66,18 @@
 	</div>
 </div>
 <!-- End services Section -->
+
+
+
+<script>
+	document.addEventListener("DOMContentLoaded", (event) => {
+		document.querySelector("li.page-item.active > span").style.backgroundColor="gray"
+		document.querySelector("li.page-item.active > span").style.borderColor="gray"
+		document.querySelector("li.page-item.disabled > span").style.backgroundColor="gray"
+		document.querySelector("li.page-item.disabled > span").style.borderColor="gray"
+	
+		document.querySelector(".pagination").style.margin=0
+	})
+</script>
 
 @include( 'partials.footer' )
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -53,7 +53,7 @@
 							<a class="nav-link" href="/page/add">Add game</a>
 						</li>
 						<li class="{{$page=='contact'?'nav-item active':''}}">
-							<a class="nav-link" href="/page/contact">Contact us</a>
+							<a class="nav-link" href="/page/contact">Contact Me</a>
 						</li>
 					</ul>
 
@@ -74,7 +74,7 @@
 					<div class="col-lg-5">
 						<div class="intro-excerpt">
 							<img src="/images/logo-hero.png" alt="" class="mb-1">
-							<p><a href="/page/games" class="btn btn-secondary me-2">All Games</a><a href="#" class="btn btn-white-outline">Explore</a></p>
+							<p><a href="/page/games" class="btn btn-secondary me-2">All Games</a><a href="/page/contact" class="btn btn-white-outline">Contact Me</a></p>
 						</div>
 					</div>
 					<div class="col-lg-7">
@@ -124,13 +124,3 @@
 				<path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
 			</svg>
 		</div>
-
-		<script>
-			document.addEventListener("DOMContentLoaded", (event) => {
-				document.querySelector("li.page-item.active > span").style.backgroundColor="gray"
-				document.querySelector("li.page-item.active > span").style.borderColor="gray"
-				document.querySelector("li.page-item.disabled > span").style.backgroundColor="gray"
-				document.querySelector("li.page-item.disabled > span").style.borderColor="gray"
-			})
-			
-		</script>
