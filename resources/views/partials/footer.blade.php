2427f8bb3f6541d0d64cab414d8dd5ffe9732df8
watawata <mohamed.leghdaich@gmail.com>
2024-09-14 20:51:09 +0100

    add copywrite footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -57,15 +57,34 @@
 
 				</div>
 
+			<div class="border-top copyright">
+				<div class="row">
+					<div class="col-lg-6">
+						<p class="text-center text-lg-start">Created by <a href="https://untree.co" style="color:#daac3a !important;">c-triangle</a> 2024.</p>
+					</div>
 
+					<div class="col-lg-6 text-center text-lg-end">
+						<ul class="list-unstyled d-inline-flex ms-auto">
+							<li class="me-4"><a href="#" style="color:#daac3a !important;">Terms &amp; Conditions</a></li>
+							<li><a href="#" style="color:#daac3a !important;">Contact Us</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
 				
+			</div>
+
+
 				<div class="wave2">
 					<svg data-name="Layer 1" viewBox="0 0 1200 120" preserveAspectRatio="none">
 						<path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
 					</svg>
 				</div>
 
-			</div>
+			
+
+
+
 			</div>
 		</footer>
 		<!-- End Footer Section -->	
